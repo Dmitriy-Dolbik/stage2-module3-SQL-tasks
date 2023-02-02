69deb05aa18eed9e1667f34267b5e7c38a560478
Dmitrii Dolbik <dda1508@gmail.com>
2023-02-03 00:45:00 +0300

    Correct solution3

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM Payment WHERE amount>=500;
 SELECT * FROM Student WHERE YEAR(CURDATE()) - YEAR(birthday) > 20;
 SELECT * FROM Student WHERE groupnumber = 10 AND YEAR(CURDATE()) - YEAR(birthday) < 20;
-SELECT * FROM Student WHERE name = 'Mike' OR (groupnumber >= 4 AND groupnumber <= 6 AND id = 1597);
+SELECT * FROM Student WHERE name = 'Mike' OR (groupnumber >= 4 AND groupnumber <= 6) OR id = 1597;
 SELECT * FROM Payment WHERE payment_date >= DATEADD(month , -8, CURDATE()) OR payment_date='2021-08-31 04:16:23.000000';
 SELECT * FROM student WHERE name LIKE 'A%';
-SELECT * FROM Student WHERE name = 'Roxi' AND groupnumber = 4 OR name = 'Tallie' AND groupnumber = 9;
+SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9) OR id = 1597;
