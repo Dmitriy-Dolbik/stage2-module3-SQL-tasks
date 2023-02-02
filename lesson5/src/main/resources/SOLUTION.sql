c3f0bf56eee8bd6d24a5dfafce0b17491b24ed23
Dmitrii Dolbik <dda1508@gmail.com>
2023-02-02 20:12:06 +0300

    Add solution2

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,6 +1,6 @@
 SELECT * FROM Payment WHERE amount>=500;
-SELECT * FROM Student WHERE DATEDIFF(YEAR, birthday, GETDATE()) > 20;
-SELECT * FROM Student WHERE groupnumber = 10 AND DATEDIFF(YEAR, birthday, GETDATE()) < 20;
-SELECT * FROM Student WHERE name = 'Mike' OR (groupnumber >= 4 AND groupnumber <=6);
-SELECT * FROM Payments WHERE DATEDIFF(MONTH, payment_date, GETDATE()) > 8;
+SELECT * FROM Student WHERE YEAR(CURDATE()) - YEAR(birthday) > 20;
+SELECT * FROM Student WHERE groupnumber = 10 AND YEAR(CURDATE()) - YEAR(birthday) < 20;
+SELECT * FROM Student WHERE name = 'Mike' OR groupnumber IN (4, 5, 6);
+SELECT * FROM Payment WHERE payment_date >= MONTH(CURDATE() - INTERVAL '8' MONTH);
 SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
